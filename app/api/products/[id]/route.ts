dd4db7c68cd64634a70dda2ac4cd22b1d6c6a201
renamcomn <acidmud@gmail.com>
2024-12-10 15:39:06 -0300

    feat: added dynamic route handler

---
+++ app/api/products/[id]/route.ts
@@ -0,0 +1,27 @@
+import { db } from "@/app/_lib/prisma"
+
+export async function GET(request: Request, { params } : { params: { id: string }}) {
+    const productId = params.id
+    const product = await db.product.findUnique({
+        where: {
+            id: productId
+        }
+    })
+
+    if(!product) {
+        return Response.json({ message: "Product not found"}, { status: 404 })
+    }
+
+    return Response.json(product, { status: 200 })
+}
+
+export async function DELETE(request: Request, { params } : { params: { id: string }}) {
+
+    await db.product.delete({
+        where: {
+            id: params.id
+        }
+    })
+
+    return Response.json({}, { status: 204 })
+}
