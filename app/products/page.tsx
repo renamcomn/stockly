4bf8be387787f6c5b68f991ab32e188f9eba0a57
renamcomn <acidmud@gmail.com>
2024-12-10 15:02:03 -0300

    refactor: added route handler

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -21,7 +21,7 @@ const ProductsPage = async () => {
           Novo produto
         </Button>
       </div>
-      <DataTable columns={productTableColumns} data={products} />
+      <DataTable columns={productTableColumns} data={JSON.parse(JSON.stringify(products))} />
     </div>
   );
 };
